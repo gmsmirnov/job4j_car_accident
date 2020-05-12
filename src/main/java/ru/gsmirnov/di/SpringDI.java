5f4d46fd3ccd3d82aa15f91211998e09d4e68b06
gsmirnov <artress@ngs.ru>
2020-05-12 19:19:04 +0700

    1. Spring DI. Запуск. [#2091]

---
+++ src/main/java/ru/gsmirnov/di/SpringDI.java
@@ -0,0 +1,40 @@
+package ru.gsmirnov.di;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Application entry point. Configures the application, by creating context and registrating dependencies.
+ *
+ * @author Gregory Smirnov (gsmirnov <artress@ngs.ru>)
+ * @version 0.1
+ * @since 2020-05-12
+ */
+public class SpringDI {
+    /**
+     * The SpringDI.class logger.
+     */
+    private static final Logger LOG = LogManager.getLogger(SpringDI.class);
+
+    /**
+     * Entry point.
+     *
+     * @param args application params.
+     */
+    public static void main(String[] args) {
+        LOG.info("Application started");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(ConsoleInput.class);
+        context.register(StartUI.class);
+        context.refresh();
+        LOG.info("Context initialized.");
+
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add("Ivan Ivanov");
+        ui.add("Petr Petrov");
+        ui.print();
+        LOG.info("Application finished");
+    }
+}
